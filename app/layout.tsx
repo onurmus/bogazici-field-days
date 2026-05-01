137db52c61ee18bfaa0e64f97c6f2dfa2601a6b7
Onur Musaoglu <onurmusaolu@gmail.com>
2026-05-02 01:18:59 +0300

    suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
-      <body className="min-h-screen bg-[#f5f0e8]">
+      <body className="min-h-screen bg-[#f5f0e8]" suppressHydrationWarning>
         {children}
         <SpeedInsights />
       </body>
